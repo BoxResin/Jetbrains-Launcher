4b3c0bcb4cf40bfa7e3dda476eaab02c92148d95
Minsuk Eom <boxresin@gmail.com>
2017-03-04 02:39:42 +0900

    Added translation messages.

--- language/EN_US.py
+++ language/EN_US.py
@@ -5,4 +5,9 @@ MSG = {
     'OPENING_IDE': 'Opening {0!s} with {1!s}...', # {0!s} is path of project, and {1!s} is IDE name.
     'INVALID_JETBRAINS_PROJECT': "It's not a valid Jetbrains project.",
     'UNKNOWN_JETBRAINS_PROJECT': "It's an unknown Jetbrains project.",
+    'MENU_TITLE': 'Open with Jetbrains IDE',
+
+    # Installer messages
+    'PAGE_PATH_INPUT_TITLE': 'Connecting to Jetbrains IDE',
+    'PAGE_PATH_INPUT_SUBTITLE': "Please input the following program's exe file.$\nThe Python Interpreter is essential, others are optional.",
 }
--- language/root.py
+++ language/root.py
@@ -4,4 +4,9 @@ MSG = {
     'OPENING_IDE': '{1!s}(으)로 {0!s}를 여는 중입니다.', # {0!s}는 프로젝트 경로, {1!s}는 IDE 이름
     'INVALID_JETBRAINS_PROJECT': '올바른 Jetbrains 프로젝트가 아닙니다.',
     'UNKNOWN_JETBRAINS_PROJECT': '알려지지 않은 Jetbrains 프로젝트입니다.',
+    'MENU_TITLE': 'Jetbrains IDE로 열기',
+
+    # Installer 메시지
+    'PAGE_PATH_INPUT_TITLE': 'Jetbrains IDE와 연결',
+    'PAGE_PATH_INPUT_SUBTITLE': '다음 프로그램의 exe 파일 경로를 입력해주세요.$\nPython Interpreter은 필수 입력 내용이며, 나머지는 옵션입니다.',
 }
