353cf183f30faab85eabdefaed6ad9040d14858d
Minsuk Eom <boxresin@gmail.com>
2017-03-04 19:58:21 +0900

    Fixed a bug that when there are no translation suitable to user, default language isn't be represented.

--- language/EN_US.py
+++ language/EN_US.py
@@ -1,14 +1,13 @@
 # Edit this file to create English translations
-from language.root import *
+from language.root import MSG
 
-MSG = {
+_LOCAL_MSG = {
     'OPENING_IDE': 'Opening {0!s} with {1!s}...', # {0!s} is path of project, and {1!s} is IDE name.
     'INVALID_JETBRAINS_PROJECT': "It's not a valid Jetbrains project.",
     'UNKNOWN_JETBRAINS_PROJECT': "It's an unknown Jetbrains project.",
     'MENU_TITLE': 'Open with Jetbrains IDE',
 
     # Installer messages
-    'PAGE_PATH_INPUT_TITLE': 'Connecting to Jetbrains IDE',
     'PAGE_PATH_INPUT_SUBTITLE': "Please input the following program's exe file.$\\n" # Use '$\\n' to put a newline.
                                 "The Python Interpreter is essential, others are optional.",
 
@@ -16,3 +15,5 @@ MSG = {
     'UNINST_DELETE_CONFIRM': 'Do you want to uninstall $(^Name)?',  # $(^Name) is program name.
     'UNINST_DELETED': '$(^Name) has been completely uninstalled.',  # $(^Name) is program name.
 }
+
+MSG.update(_LOCAL_MSG)
